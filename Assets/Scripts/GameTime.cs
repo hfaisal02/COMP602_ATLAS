810e18faf4149c824e3074d48e33d9d12bf21241
Solvee <toryn.pope@gmail.com>
2022-09-19 18:34:43 +1200

    Time imeplementation
    - Implemented Game Time tracking capability
    
    Note: Still requires a pause menu to work with

---
+++ Assets/Scripts/GameTime.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTime : MonoBehaviour
+{
+    private float playTime = 0.0f;
+    private bool runTimer = false;
+
+    //Test Variable
+    private bool gameRunningTest = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameRunningTest == true)
+        {
+            TimerStart();
+
+            if(runTimer)
+            {
+                playTime += Time.deltaTime;
+                TimePlayed();
+            }
+        }
+        else if(gameRunningTest == false)
+        {
+            StopTimer();
+        }
+    }
+
+    void TimePlayed()
+    {
+        int seconds = (int)(playTime % 60);
+        int minutes = (int)(playTime / 60) % 60;
+        int hours = (int)(playTime / 3600) % 24;
+
+        string displayTime = string.Format("{0:0}:{1:00}:{2:00}", hours, minutes, seconds);
+
+        Debug.Log(displayTime);
+    }
+
+    void TimerStart()
+    {
+        runTimer = true;
+        //Test code
+        //Debug.Log("Timer is running...");
+    }
+
+    void StopTimer()
+    {
+        runTimer = false;
+        ////Test code
+        Debug.Log("Timer is stopped..");
+    }
+}
