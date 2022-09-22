3659b66cf5876233539af1c19aa7d4aa2494f4fb
monitosh <thaker.moni@gmail.com>
2022-09-23 06:51:45 +1200

    Implemented Weapon Placeholder & Rotation
    Added a temporary weapon object to the player and implemented rotation so that it aims towards the mouse cursor or joystick direction.

---
+++ Assets/Scripts/WeaponRotation.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WeaponRotation : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    private Vector3 mouseVector;
+    private PlayerInput playerInput;
+    public Transform aim;
+
+    void Start()
+    {
+        playerInput = FindObjectOfType<PlayerInput>();
+    }
+    void Update()
+    {
+        GetMouseInput();
+        WeaponAnimation();
+    }
+
+    void GetMouseInput()
+    {        
+        if(playerInput.currentControlScheme == "Gamepad")
+        {
+            mouseVector = (playerInput.actions["Aim"].ReadValue<Vector2>()).normalized;            
+        }
+        else
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(playerInput.actions["Aim"].ReadValue<Vector2>());
+            mouseVector = (mousePosition - transform.position).normalized;
+        }        
+    }
+    void WeaponAnimation()
+    {
+        float weaponAngle = Mathf.Atan2(mouseVector.y, mouseVector.x) * Mathf.Rad2Deg;
+        aim.eulerAngles = new Vector3(0, 0, weaponAngle);
+    }
+}
