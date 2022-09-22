324830e497966831e6922a5edfd2b315a1581a98
hfaisal02 <106284237+hfaisal02@users.noreply.github.com>
2022-09-22 21:21:14 +1200

    Test
    Test

--- Assets/Scripts/UI Scripts/UISetName.cs
+++ Assets/Scripts/UI Scripts/UISetName.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UISetName : MonoBehaviour
 {
+    public Text object_text;
+    public InputField nameDisplay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //object_text.text = PlayerData.playerName;
+    }
+
+    public void SetName()
+    {
+        PlayerData.playerName = object_text.text;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        object_text = nameDisplay.text;
     }
 }
